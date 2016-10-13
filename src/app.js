cd8bcc2753901068bbebc1c441b752295cbbcf18
haruelrovix <HavitChoirul.Rovik@mitrais.com>
2016-10-13 15:02:07 +0700

    Communicate child to parent

--- src/app.js
+++ src/app.js
@@ -51,8 +51,29 @@ var BugAdd = React.createClass({
     render: function() {
         console.log("Rendering BugAdd");
         return (
-            <div>A form to add a new bug would come here.</div>
+            <div>
+                <form name="bugAdd">
+                    <input type="text" name="owner" placeholder="Owner" />
+                    <input type="text" name="title" placeholder="Title" />
+                    <button onClick={this.handleSubmit}>Add Bug</button>
+                </form>
+            </div>
         )
+    },
+
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var form = document.forms.bugAdd;
+        this.props.addBug({
+            owner: form.owner.value,
+            title: form.title.value,
+            status: 'New',
+            priority: 'P1'
+        });
+
+        // Clear the form for the next input
+        form.owner.value = "";
+        form.title.value = "";
     }
 });
 
@@ -73,23 +94,16 @@ var BugList = React.createClass({
                 <BugFilter />
                 <hr />
                 <BugTable bugs={this.state.bugs} />
-                <button onClick={this.testNewBug}>Add Bug</button>
                 <hr />
-                <BugAdd />
+                <BugAdd addBug={this.addBug} />
             </div>
         )
-    },
-
-    testNewBug: function() {
-        var nextId = this.state.bugs.length + 1;
-        this.addBug({
-            id: nextId, priority: 'P2', status:'New', owner:'Haru', title:'Warning on console'
-        })
     },
 
     addBug: function(bug) {
         console.log("Adding bug:", bug);
         var bugsModified = this.state.bugs.slice();
+        bug.id = this.state.bugs.length + 1;
         bugsModified.push(bug);
         this.setState({
             bugs: bugsModified
