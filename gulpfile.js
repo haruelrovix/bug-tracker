2f831f01676b3d87efa7665086f68ab331831bd8
haruelrovix <HavitChoirul.Rovik@mitrais.com>
2016-10-18 10:28:40 +0700

    Install Watchify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 gulp.task('bundle', function() {
     return browserify('src/app.js')
@@ -8,4 +9,27 @@ gulp.task('bundle', function() {
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(gulp.dest('static/'));
+});
+
+gulp.task('watch', function() {
+    var b = browserify({
+        entries: ['src/app.js'],
+        cache: {},
+        packageCache: {},
+        plugin: ['watchify']
+    });
+
+    b.on('update', makeBundle);
+
+    function makeBundle() {
+        b.transform('babelify', {presets: 'react'})
+            .bundle()
+            .pipe(source('bundle.js'))
+            .pipe(gulp.dest('static/'));
+    }
+
+    // We have to call bundle once to kick it off.
+    makeBundle();
+
+    return b;
 });
