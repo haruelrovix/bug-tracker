5cfaa6e3dfa17c24f46226e2ca18ba14195052a3
haruelrovix <HavitChoirul.Rovik@mitrais.com>
2016-10-18 13:55:19 +0700

    Add React Router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router').Router;
+var Route = require('react-router').Route;
+var Redirect = require('react-router').Redirect;
+var hashHistory = require('react-router').hashHistory;
 
 var BugList = require('./BugList');
 
+var NoMatch = React.createClass({
+    render: function() {
+        return (
+            <h2>No match for the route</h2>
+        );
+    }
+});
+
 ReactDOM.render(
-    <BugList />,
+    <Router history={hashHistory}>
+        <Route path="/bugs" component={BugList} />
+        <Redirect from="/" to="/bugs" />
+        <Route path="*" component={NoMatch} />
+    </Router>,
     document.getElementById('main')
 );
