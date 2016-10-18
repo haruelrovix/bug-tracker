f52baf2d4af0f64309909b95976a998c51237221
haruelrovix <HavitChoirul.Rovik@mitrais.com>
2016-10-18 07:22:40 +0700

    GET API

--- webapp.js
+++ webapp.js
@@ -3,6 +3,15 @@ var app = express();
 
 app.use(express.static('static'));
 
+var bugData = [
+  {id: 1, priority: 'P1', status:'Open', owner:'Havit', title:'App crash on open'},
+  {id: 2, priority: 'P2', status:'New', owner:'Rovik', title:'Missaligned border on panel'}
+];
+
+app.get('/api/bugs', function(req, res) {
+  res.status(200).send(JSON.stringify(bugData));
+});
+
 var server = app.listen(3000, function() {
   var port = server.address().port;
   console.log("Started server at port", port);
